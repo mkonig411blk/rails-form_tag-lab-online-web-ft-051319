65afb094813c57a2f1603fe26263044505d2c47a
Margot Konig <margot.konig@blackrock.com>
2019-07-22 23:08:51 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,5 +7,13 @@ class StudentsController < ApplicationController
   def show
     @student = Student.find(params[:id])
   end
+  
+  def new
+  end
+  
+  def create
+  Student.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+end
 
 end
